fdd6331e7c0154ddedeed127be2c8861b0bd0eff
Safril Yusup <safrilyusuf05@gmail.com>
2025-08-26 11:43:53 +0900

    aplikasi biodata

---
+++ KULIAH/DART/Project 1/lib/latihan1.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "ilham";
+  int age = 20;
+  double berat = 60.5;
+  num tinggi = 170.5;
+  dynamic Prodi = "Teknik Informatika";
+
+  print("Nama : $name");
+  print("Umur : $age");
+  print("Berat : $berat");
+  print("Tinggi : $tinggi");
+  print("Prodi : $Prodi");
+}
